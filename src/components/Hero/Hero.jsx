72e7385f08c384556fe68224660267b31f496ce2
Art1488em <artemdavydov401@gmail.com>
2024-11-25 17:06:42 +0500

    Add files via upload

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Hero.scss'
+
+const Hero = () => {
+  return (
+    <>
+    <div className="wrap">
+        <div className="box">
+            
+        </div>
+        <div className="box"></div>
+    </div>
+    </>
+  )
+}
+
+export default Hero
